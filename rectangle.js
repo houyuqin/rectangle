9b5120a118a307aa1b52692f3e3294fa5c9937bf
houyuqin <1738519809@qq.com>
2020-02-21 13:51:39 +0800

    结果保留2位小数

--- rectangle.js
+++ rectangle.js
@@ -8,17 +8,16 @@ $(function(){
         $area = $("#area")
 
     $btnCal.click(function(){
-       
-        //get value
-        var w = Number($width.val()),
-            h = Number($height.val());
-    
+      //get value
+      var w = Number($width.val()),
+          h = Number($height.val());
 
-    //calculate
-    var p = 2*(w+h),
-        a = w*h;
-    //output
-    $perimeter.val(p);
-    $area.val(a);
+      //calculate
+      var p = 2*(w+h),
+          a = w*h;
+
+      //output
+      $perimeter.val(p.toFixed(2));
+      $area.val(a.tofix(2));
     }); 
 })
