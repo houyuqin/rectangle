a9e2dee3a08995a329ca0f528782df202da133aa
houyuqin <1738519809@qq.com>
2020-02-21 13:42:05 +0800

    add 0.1

---
+++ rectangle.js
@@ -0,0 +1,24 @@
+$(function(){
+    /*calc button click event*/
+    //get dom elem
+    var $width = $("#width"),
+        $height = $("#height"),
+        $btnCal = $("#calculate"),
+        $perimeter = $("#perimeter"),
+        $area = $("#area")
+
+    $btnCal.click(function(){
+       
+        //get value
+        var w = Number($width.val()),
+            h = Number($height.val());
+    
+
+    //calculate
+    var p = 2*(w+h),
+        a = w*h;
+    //output
+    $perimeter.val(p);
+    $area.val(a);
+    }); 
+})
